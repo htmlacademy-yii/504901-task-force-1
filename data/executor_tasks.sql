5f319b62bb189bc654bdc6451f021afa2b80c69d
Елена <tyshenko61@mail.ru>
2020-07-17 21:47:13 +0500

    Первый шаг на большом пути

--- data/executor_tasks.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO executor_tasks (id_task, date_of_appointment, id_executor) VALUES
-('8','2019-04-02','10'),
-('9','2019-03-29','11'),
-('10','2019-05-02','12');
--- data/statuses.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO statuses (name, translation) VALUES
-('new','Новое'),
-('canceled','Отменено'),
-('in_work','В работе'),
-('performed','Выполнено'),
-('failed','Провалено');
